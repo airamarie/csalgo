3ec344f6b4131577ef80a7c83da1381e341dd3d9
AIRA MARIE MANDIA <APMANDIA@fit.edu.ph>
2016-09-10 21:09:23 +0800

    Added LexicoGraphicGenerator

---
+++ LexicoGraphicGenerator/src/lexicographicgenerator/LexicoGraphicGenerator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lexicographicgenerator;
+
+/**
+ *
+ * @author apmandia
+ */
+public class LexicoGraphicGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String charset = "ABC";
+        for (int i=0; i<charset.length(); i++){
+            System.out.println(charset.charAt(i));
+        }
+    }
+    
+}
