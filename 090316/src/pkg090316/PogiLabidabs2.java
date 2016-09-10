960d526d89e94b8b1d8cb8c5901b91b75fc6c642
AIRA MARIE MANDIA <APMANDIA@fit.edu.ph>
2016-09-10 20:53:22 +0800

    Altered Code in Labidabs2

--- 090316/src/pkg090316/PogiLabidabs2.java
+++ 090316/src/pkg090316/PogiLabidabs2.java
@@ -3,28 +3,31 @@ package pkg090316;
 public class PogiLabidabs2 {
 
     public static void main(String[] args) {
-        System.out.println("Sorting numbers 8, 0, 2, 1, 5.");
+        System.out.print("Sorting numbers: ");
         int[] yeh = {8, 0, 2, 1, 5};
-        int x, y, min, tem;
-
-        for (x = 0; x < yeh.length; x++) {
-            min = x;
-
-            for (y = x + 1; y < yeh.length; y++) {
+        int tem = 0;
+        for (int i = 0; i < yeh.length; i++) {
+            System.out.print(yeh[i] + " ");
+        }
+        System.out.println("");
+        for (int x = 0; x < yeh.length; x++) {
+            int min = x;
+            for (int y = x + 1; y < yeh.length; y++) {
                 if (yeh[y] < yeh[min]) {
                     min = y;
-
                 }
-
-                tem = yeh[x];
-                yeh[x] = yeh[min];
-                yeh[min] = tem;
-
             }
+            tem = yeh[x];
+            yeh[x] = yeh[min];
+            yeh[min] = tem;
+            for (int i = 0; i < yeh.length; i++) {
+                System.out.print(yeh[i] + " ");
+            }
+            System.out.println("");
         }
-
-        System.out.print(yeh[x]);
+        System.out.print("Array contains: ");
+        for (int i = 0; i < yeh.length; i++) {
+            System.out.print(yeh[i] + " ");
+        }
     }
-
 }
-
